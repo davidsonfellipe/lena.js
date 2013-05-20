d798120f46c2f52ff237e9cc26357bf843c135d1
Davidson Fellipe <email@fellipe.com>
2013-05-20 00:44:47 -0300

    sepia filter

--- assets/js/filters/sepia.js
+++ assets/js/filters/sepia.js
@@ -1,4 +1,4 @@
-LenaJS.grayscale = function(pixels, args) {
+LenaJS.sepia = function(pixels, args) {
 
   var d = pixels.data;
 
@@ -8,9 +8,13 @@ LenaJS.grayscale = function(pixels, args) {
         g = d[i+1],
         b = d[i+2];
 
-    var v = 0.2126*r + 0.7152*g + 0.0722*b;
+    var v = 0.3*r + 0.59*g + 0.11*b;
 
     d[i] = d[i+1] = d[i+2] = v;
+
+    d[i] += 40;
+    d[i+1] += 20;
+    d[i+2] -= 20;
   }
 
   return pixels;
--- assets/js/runner/runner.js
+++ assets/js/runner/runner.js
@@ -1,20 +1,15 @@
-
-
 window.onload = function() {
 
-    // document.getElementById('filter').addEventListener("change", function(){
+    document.getElementById('filter').addEventListener("change", function(){
         var img = document.getElementById("lena");
-        // var Filter = new LenaJS;
 
-        var c = document.getElementById("grayscale");
+        var idata = LenaJS.filterImage(LenaJS[this.value], img);
 
-        var idata = LenaJS.filterImage(LenaJS.sepia, img);
+        var c = document.getElementById("canvas");
         c.width = idata.width;
         c.height = idata.height;
         var ctx = c.getContext('2d');
         ctx.putImageData(idata, 0, 0);
-        // runFilter('grayscale', LenaJS.grayscale);
-        // LenaJS.filterImage(LenaJS.grayscale, img);
-    // }, false);
+    }, false);
 
 };
