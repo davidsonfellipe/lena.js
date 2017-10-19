2b3ecde7080ed5dcc0e5dc56a5af2cbb082339e7
Romain SOMMERARD <rsommerard@gmail.com>
2017-10-19 20:52:31 +0200

    Add a vertical mirror filter.

--- dist/lena.js
+++ dist/lena.js
@@ -304,6 +304,38 @@ LenaJS.lowpass5 = function(pixels, args) {
   return LenaJS.convolution(pixels, operator);
 };
 
+LenaJS.mirror = function(pixels, args) {
+  const tmp = [];
+  const width = (pixels.width * 4);
+
+  for (let h = 0; h < pixels.height; h++) {
+    const offset = h * width;
+
+    for (let w = 0; w < pixels.width / 2; w++) {
+      const pos = w * 4;
+      const pxl = pos + offset;
+      const lastPxl = width - pos - 4 + offset;
+
+      tmp[0] = pixels.data[pxl];
+      tmp[1] = pixels.data[pxl + 1];
+      tmp[2] = pixels.data[pxl + 2];
+      tmp[3] = pixels.data[pxl + 3];
+
+      pixels.data[pxl] = pixels.data[lastPxl];
+      pixels.data[pxl + 1] = pixels.data[lastPxl + 1];
+      pixels.data[pxl + 2] = pixels.data[lastPxl + 2];
+      pixels.data[pxl + 3] = pixels.data[lastPxl + 3];
+
+      pixels.data[lastPxl] = tmp[0];
+      pixels.data[lastPxl + 1] = tmp[1];
+      pixels.data[lastPxl + 2] = tmp[2];
+      pixels.data[lastPxl + 3] = tmp[3];
+    }
+  }
+
+    return pixels;
+}
+
 // http://en.wikipedia.org/wiki/Prewitt_operator
 
 LenaJS.prewittHorizontal = function(pixels, args) {
