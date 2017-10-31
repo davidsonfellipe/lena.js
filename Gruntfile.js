b0cbd3b502949f0bbd3ebffeef5d684c2f671e85
Davidson Fellipe <email@fellipe.com>
2017-10-31 00:32:07 -0400

    (lint): fix eslint for Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,18 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     meta: {
-        banner: "/*\n" +
-            " *  <%= pkg.name %> - v<%= pkg.version %>\n" +
-            " *  <%= pkg.description %>\n" +
-            " *  <%= pkg.homepage %>\n" +
-            " *\n" +
-            " *  Made by <%= pkg.author %>\n" +
-            " *  Under <%= pkg.licenses[0].type %> License\n" +
-            " */\n"
+      banner: '/*\n' +
+            ' *  <%= pkg.name %> - v<%= pkg.version %>\n' +
+            ' *  <%= pkg.description %>\n' +
+            ' *  <%= pkg.homepage %>\n' +
+            ' *\n' +
+            ' *  Made by <%= pkg.author %>\n' +
+            ' *  Under <%= pkg.licenses[0].type %> License\n' +
+            ' */\n'
     },
     concat: {
       options: {
-        banner: "<%= meta.banner %>"
+        banner: '<%= meta.banner %>'
       },
       basic: {
         src: ['lib/lena.js', 'lib/util/*.js', 'lib/filters/*.js'],
@@ -21,8 +21,8 @@ module.exports = function(grunt) {
       },
       extras: {
         src: ['demo/assets/js/vendor/jquery/*.js',
-              'demo/assets/js/vendor/jquery-drag-drop-plugin/*.js',
-              'demo/assets/js/vendor/nvd3/*.js'],
+          'demo/assets/js/vendor/jquery-drag-drop-plugin/*.js',
+          'demo/assets/js/vendor/nvd3/*.js'],
         dest: 'demo/assets/js/vendor/all.js'
       },
     },
@@ -50,17 +50,17 @@ module.exports = function(grunt) {
         },
       },
     }
-  });
+  })
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify')
 
   // Load the plugin that provides the "concat" task.
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat')
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch')
 
   // Default task(s).
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat'])
 
-};
+}
