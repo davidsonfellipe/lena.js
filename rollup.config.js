7ece74bf324a95141764871353f6e3472c46b93d
Davidson Fellipe <email@fellipe.com>
2020-07-26 22:50:33 -0400

    refactor(es6): migrate code to ES6 and bundle process to rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import { terser } from 'rollup-plugin-terser'
+import { version } from './package.json'
+
+const BANNER = `
+/*
+ *  lena.js - ${version}
+ *  Library for image processing <https://github.com/davidsonfellipe/lena-js/>
+ *
+ *  Made by Davidson Fellipe.
+ *  Released under MIT License
+ */
+`
+
+export default {
+  input: './lib/index.js',
+  output: [
+    {
+      banner: BANNER,
+      file: './dist/bundle.js',
+      format: 'cjs',
+    },
+    {
+      banner: BANNER,
+      file: './dist/bundle.min.js',
+      format: 'iife',
+      name: 'LenaJS',
+      plugins: [terser()],
+    },
+  ],
+}
